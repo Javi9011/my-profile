4fcd25ab0c0bd3b5532b0aa087ba224fe819e1cc
Nelson Javier <javo9011@gmail.com>
2025-06-02 19:58:12 -0400

    Primer commit: estructura base del portfolio

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ padding: "2rem", fontFamily: "sans-serif" }}>
+      <h1>Hola, soy Nelson Amaya</h1>
+      <p>Desarrollador Full Stack</p>
+      <ul>
+        <li>Python</li>
+        <li>Java</li>
+        <li>JavaScript</li>
+        <li>React</li>
+        <li>Angular</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+function Header() {
+    return (
+      <header>
+        <h1>Mi Portfolio</h1>
+        <nav>
+          <a href="#about">Sobre mí</a>
+          <a href="#projects">Proyectos</a>
+          <a href="#contact">Contacto</a>
+        </nav>
+      </header>
+    );
+  }
+  
+  export default Header;
+  
